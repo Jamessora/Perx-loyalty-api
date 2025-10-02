f87ac46d56a469ed2e918d11a40a60a50d791648
unknown <cadaciojerico@gmail.com>
2025-10-02 18:04:42 +0800

    Create api clients table for auth token.

---
+++ db/migrate/20251002095947_create_api_clients.rb
@@ -0,0 +1,11 @@
+class CreateApiClients < ActiveRecord::Migration[8.0]
+  def change
+    create_table :api_clients do |t|
+      t.string :name,  null: false
+      t.string :token, null: false
+
+      t.timestamps
+    end
+    add_index :api_clients, :token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_02_095947) do
+  create_table "api_clients", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_api_clients_on_token", unique: true
+  end
 end
